f8801d51a24ca8815f2db7787c44b3a42822d63e
dfuentes22 <dvf5104@gmail.com>
2016-07-04 14:22:48 -0400

    step 9 completed, waiting on 10

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,26 @@
 import {Component} from 'angular2/core';
+import  {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
+
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
+
 })
 
-export class AppComponent {}
+export class AppComponent {
+	mainHeading =  Config.MAIN_HEADING;
+	videos: Array<Video>;
+
+	constructor(){
+		this.videos = [
+			new Video(1, "Installing Django","qgGIqRFvFFk", "How to install Django"),
+			new Video(2, "Surviving the Wilderness","eXmyEAC0mpY", "Bucky goes hiking."),
+		]
+	}
+}
+
